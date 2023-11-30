e683c6f22e252664179b36aff99d8712be778a77
Yuga Samuel <yugasamuel2002@gmail.com>
2023-11-30 13:08:20 +0700

    Initial Commit

---
+++ WeatherBuddy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherBuddy
+//
+//  Created by Yuga Samuel on 30/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherBuddy/WeatherBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherBuddyApp.swift
+//  WeatherBuddy
+//
+//  Created by Yuga Samuel on 30/11/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
