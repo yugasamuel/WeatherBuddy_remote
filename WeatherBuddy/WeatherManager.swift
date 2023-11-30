c757923dfe4efd3bfd117a58a674069fcd3f7da1
Yuga Samuel <yugasamuel2002@gmail.com>
2023-11-30 15:03:26 +0700

    Add properties based on weather data

--- WeatherBuddy/WeatherManager.swift
+++ WeatherBuddy/WeatherManager.swift
@@ -24,4 +24,30 @@ import WeatherKit
             print("Failed to get weather data. \(error)")
         }
     }
+    
+    var condition: String {
+        guard let condition = weather?.currentWeather.condition else { return "--" }
+        
+        return condition.description
+    }
+    
+    var icon: String {
+        guard let iconName = weather?.currentWeather.symbolName else { return "--" }
+        
+        return iconName
+    }
+    
+    var temperature: String {
+        guard let temp = weather?.currentWeather.temperature else { return "--" }
+        let convert = temp.converted(to: .celsius).value
+        
+        return String(Int(convert)) + "°C"
+    }
+    
+    var humidity: String {
+        guard let humidity = weather?.currentWeather.humidity else { return "--" }
+        let computedHumidity = humidity * 100
+        
+        return String(Int(computedHumidity)) + "%"
+    }
 }
