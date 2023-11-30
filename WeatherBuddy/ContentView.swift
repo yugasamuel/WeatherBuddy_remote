fe34b2db5bd837062338ac3eb297b526d58509ea
Yuga Samuel <yugasamuel2002@gmail.com>
2023-11-30 15:56:37 +0700

    Setup view for user

--- WeatherBuddy/ContentView.swift
+++ WeatherBuddy/ContentView.swift
@@ -6,16 +6,29 @@
 //
 
 import SwiftUI
+import CoreLocation
 
 struct ContentView: View {
+    @State private var weatherManager = WeatherManager()
+    
+    let jakarta = CLLocation(latitude: -6.21462, longitude: 106.84513)
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 12) {
+            Image(systemName: weatherManager.icon)
+                .font(.largeTitle)
+                .shadow(radius: 2)
+                .padding()
+            Text("Temperature: \(weatherManager.temperature)")
+            Text("Humidity: \(weatherManager.humidity)")
         }
-        .padding()
+        .onAppear {
+            Task {
+                await weatherManager.getWeather(lat: jakarta.coordinate.latitude,
+                                                long: jakarta.coordinate.longitude)
+            }
+        }
+        .font(.title3)
     }
 }
 
